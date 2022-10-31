8ed0d3c89eccce85c2d90e7b258025b54bb0d894
Gangqiao Kuang <gqkuang@jlsemi.com>
2022-10-31 10:56:28 +0800

    feat: fix complie error for python3

--- python/__getattr__/adapter_wrapper.py
+++ python/__getattr__/adapter_wrapper.py
@@ -5,16 +5,16 @@
 
 class adaptee(object):
     def foo(self):
-        print 'foo in adaptee'
+        print('foo in adaptee')
     def bar(self):
-        print 'bar in adaptee'
+        print('bar in adaptee')
 
 class adapter(object):
     def __init__(self):
         self.adaptee = adaptee()
 
     def foo(self):
-        print 'foo in adapter'
+        print('foo in adapter')
         self.adaptee.foo()
 
     def __getattr__(self, name):
