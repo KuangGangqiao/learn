d9b480ea1ac97b64c36cc951a91320b5413fdf06
Gangqiao Kuang <gqkuang@jlsemi.com>
2023-07-31 12:06:17 +0800

    feat(c): add pack struct demo

--- c/hand_and_struct/demo4.c
+++ c/hand_and_struct/demo4.c
@@ -30,17 +30,34 @@ typedef struct {
 	char name[200];
 }__attribute__((packed))b;
 
+void test(char *x)
+{
+	printf("x:%d\n", x[0]);
+	printf("addr_x:%p\n", x);
+}
+
 int main()
 {
 	A test0;
 	B test1;
 	a test2;
 	b test3;
+	char *h;
+
+	test3.age = 1;
+	test3.x = 2;
+	test3.num = 10;
+	test3.name[0] = 'w';
 	printf("size: %ld\n", sizeof(test0));
 	printf("size: %ld\n", sizeof(test1));
 	printf("size: %ld\n", sizeof(test2));
 	printf("size: %ld\n", sizeof(test3));
 	
+	printf("age_addr: %p\n", &test3.age);
 	printf("num_addr: %p\n", &test3.num);
 	printf("arr_addr: %p\n", &test3.name[0]);
+	h = (char*)&test3;
+	printf("addr: %p\n", &h[6]);
+	printf("h[0]: %d\n", h[6]);
+	test(h);
 }
